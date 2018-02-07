56d2db7b53ea755b48cfc0cff54b119ada8baa69
Stanimir7 <genovs6@students.rowan.edu>
2018-02-06 19:39:12 -0500

    Update WeatherData.java

--- IndirectCouplingObserver/WeatherData.java
+++ IndirectCouplingObserver/WeatherData.java
@@ -1,42 +1,10 @@
+import lombok.*;
 /**
  * WeatherData class has and maintains the temp, humidity, and pressure
  */
+@Data
 public class WeatherData {
-
     private float temperature;
     private float humidity;
     private float pressure;
-
-    public WeatherData() {
-        this.temperature = 0.0f;
-        this.humidity = 0.0f;
-        this.pressure = 0.0f;
-    }
-
-    public float getTemperature() {
-        return temperature;
-    }
-
-    public float getHumidity() {
-        return humidity;
-    }
-
-    public float getPressure() {
-        return pressure;
-    }
-
-    public WeatherData setTemperature(float temperature) {
-        this.temperature = temperature;
-        return this;
-    }
-
-    public WeatherData setHumidity(float humidity) {
-        this.humidity = humidity;
-        return this;
-    }
-
-    public WeatherData setPressure(float pressure) {
-        this.pressure = pressure;
-        return this;
-    }
 }
