75fe1ca10a96f41577063cf7a461c4c0c6d78abc
Stanimir7 <genovs6@students.rowan.edu>
2018-04-10 22:49:04 -0400

    tested and fixed to work with Lombok

--- IndirectCouplingObserver/WeatherData.java
+++ IndirectCouplingObserver/WeatherData.java
@@ -7,4 +7,15 @@ public class WeatherData {
     private float temperature;
     private float humidity;
     private float pressure;
+    
+    /**
+     * Really wanted to use lombok, but eclipse is causing problems with unspecified constructors
+     */
+    public WeatherData(float temperature, float humidity, float pressure){
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.pressure = pressure;
+    }
+    
+    public WeatherData() {}
 }
