d82941d80918269ccfbba453e839c0d440f3499c
Gene Koval <kovale8@students.rowan.edu>
2018-04-24 04:13:26 -0400

    Messages are now sent with a POST request

--- TextMessage/src/main/java/sms/SmsController.java
+++ TextMessage/src/main/java/sms/SmsController.java
@@ -2,7 +2,9 @@ package sms;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,9 +17,14 @@ public class SmsController {
     JdbcTemplate jdbcTemplate;
 
     @RequestMapping("/send")
+    public String send() {
+        return "Send a message with a POST request!";
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/send")
     public String send(
             @RequestParam(value="contact") String contact,
-            @RequestParam(value="message", defaultValue="") String message) {
+            @RequestBody String message) {
         String phone = (String) jdbcTemplate.queryForObject(String.format(
             "SELECT number FROM phone_numbers WHERE name = '%s'",
             contact
